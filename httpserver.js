39d8b5555c9b97f76d67a6dbc4700c761ef9d84a
Dewclaw <dgallo519@gmail.com>
2019-07-13 20:46:28 -0400

    Clients can communicate with eachother, messages broadcast to all clients.

--- httpserver.js
+++ httpserver.js
@@ -2,6 +2,7 @@ let http = require('http')
 let fs = require('fs')
 let WebSocket = require('ws')
 let index = fs.readFileSync('./index.html');
+let chatLog = []
 
 let server = http.createServer((request,response)=>{
     response.write(index)
@@ -13,12 +14,26 @@ let wss = new WebSocket.Server({port : 8080}, ()=>{
     console.log("WSS Listening")
 });
 wss.on('connection', (ws)=>{
+    
+    for(var i = 0; i < chatLog.length; i++){
+        ws.send(chatLog[i])
+    }
+    console.log(chatLog)
+    
     ws.on('message',(message)=>{
         console.log('received : %s', message)
         console.log("Echoing Message Back!")
-        ws.send(message)
+        console.log(wss.clients)
+        wss.clients.forEach((client)=>{
+            if(client.readyState === WebSocket.OPEN){
+                client.send(message)
+            }
+        })
+
+        chatLog.push(message);
+        // ws.send(`${chatLog[chatLog.length - 1]}`)
     })
-    ws.send('Something!')
+    ws.send('---------- Chat Server Established -------- ')
 })
 wss.on('close', (ws)=>{
     ws.send("Close Connection")
